29fea17bfba7a72557c9f1b53b2468658be863a2
Andrej Rypo <xrypak@gmail.com>
2024-08-09 10:43:04 +0200

    benchmarks
    (well, sort of)

--- tests/support/tests.bootstrap.php
+++ tests/support/tests.bootstrap.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
+use Dakujem\Toru\Dash;
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 //
 // Put common test stuff into this script (autoloaded via composer).
 //
+function _dash(iterable $input): Dash
+{
+    return Dash::collect($input);
+}
 
 //(function () {
 //    //
